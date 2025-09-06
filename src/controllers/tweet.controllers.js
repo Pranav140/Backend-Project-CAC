111184844d67252ce8baef69d8e174ab11781380
Pranav <gargpranav851@gmail.com>
2025-09-06 16:52:51 +0530

    feat: implement createTweet function with validation and response handling

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -5,9 +5,27 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+const createTweet = asyncHandler(async(req,res)=>{
+        const {content} = req.body
+
+        if(!content ||content?.trim()===""){
+            throw new ApiError(400,"Content is required")
+        }
+        const tweet = await Tweet.create({
+            content,
+            owner:req.user?._id
+        })
+
+        if(!tweet){
+            throw new ApiError(500,"failed to create tweet")
+        }
+
+        return res.
+        status(201)
+        .json(
+            new ApiResponse(201,tweet,"Tweet created successfully")
+        )
+    })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
