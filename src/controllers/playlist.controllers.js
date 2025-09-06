33ad3d2a96ae1e8594f280a40aaef882102c9002
Pranav <gargpranav851@gmail.com>
2025-09-06 18:27:52 +0530

    feat: implement playlist retrieval by ID with validation and error handling

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -51,7 +51,19 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const { playlistId } = req.params
-    //TODO: get playlist by id
+    if(!playlistId || !isValidObjectId(playlistId)){
+        throw new ApiError(400,"Invalid playlist")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if(!playlist){
+        throw new ApiError(404,"Playlist not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,playlist,"Playlist fetched successfully")
+    )
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
