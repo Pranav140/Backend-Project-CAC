9f0b8a99d01c144169e1be3ae53c360a09690119
Pranav <gargpranav851@gmail.com>
2025-09-08 12:19:56 +0530

    feat(subscription): implement toggle subscription functionality
    Add validation checks for channelId and user subscription status
    Handle both subscription and unsubscription cases
    Return appropriate response with success message

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -9,6 +9,43 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    if(!channelId || !isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channelId");
+    }
+
+    const channel = await User.findById(channelId)
+
+    if(!channel){
+        throw new ApiError(404,"Channel not found")
+    }
+
+    if(channel._id.toString()===req.user?._id.toString()){
+        throw new ApiError(400,"You cannot subscribe yourself")
+    }
+
+    const subscriptionExists = await Subscription.findOne({
+        subscriber:req.uset?._id,
+        channel:channelId
+    });
+
+    let subscription;
+    let message;
+
+    if(subscriptionExists){
+        await Subscription.findByIdAndDelete(subscriptionExists._id);
+        message = "Unsubscribed successfully";
+        subscription = null;
+    }else{
+        subscription  = await Subscription.create({
+            subscriber:res.user._id,
+            channel:channelId
+        });
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,subscription,"Subscribed successfully")
+    );
 })
 
 // controller to return subscriber list of a channel
